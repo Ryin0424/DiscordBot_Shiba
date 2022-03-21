207e33d6d73f2b56720032cbfef20af8b3355a8c
阿陰 <a30729522@gmail.com>
2022-03-21 09:31:58 +0800

    [hotfix] 移除部分 code

--- bot.js
+++ bot.js
@@ -312,15 +312,6 @@ client.on('message', msg => {
   function Omikuji(msg) {
     const fortune = ['大吉', '大兇', '中吉', '中兇', '小吉', '小兇', '末吉', '沒吉', '沒兇', '吉', '兇', '柴曰：不可說', '施主這個問題你還是別深究了吧'];
     let answer = fortune[getRandom(fortune.length)];
-    // let emoticon = ''; // 顏文字
-    // if (answer.indexOf('吉') > 0){
-    //   emoticon = 'ヽ(●´∀`●)ﾉ';
-    // } else if (answer.indexOf('兇') > 0) {
-    //   emoticon = '(›´ω`‹ )';
-    // } else {
-    //   emoticon = '';
-    // }
-    // msg.reply(`\n今日運勢： ${answer}`);
     let codeArea = '```';
     let flag1 = "　　   ○ ＿＿＿＿";
     let flag2 = "　　   ∥　　　　|";
