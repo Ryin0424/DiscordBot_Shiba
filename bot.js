93941e2155be9876aaa74046343e1f33a1836f34
阿陰 <a30729522@gmail.com>
2022-03-21 13:06:29 +0800

    [update] 增加籤筒項目

--- bot.js
+++ bot.js
@@ -310,12 +310,12 @@ client.on('message', msg => {
 
   // 柴運勢
   function Omikuji(msg) {
-    const fortune = ['大吉', '大兇', '中吉', '中兇', '小吉', '小兇', '末吉', '沒吉', '沒兇', '吉', '兇', '柴曰：不可說', '施主這個問題你還是別深究了吧'];
+    const fortune = ['大吉', '大兇', '中吉', '中兇', '小吉', '小兇', '末吉', '沒吉', '沒兇', '吉', '兇', '柴曰：不可說', '施主這個問題你還是別深究了吧', '超吉', '究吉', '兇貴'];
     let answer = fortune[getRandom(fortune.length)];
     let codeArea = '```';
     let flag1 = "　　   ○ ＿＿＿＿";
     let flag2 = "　　   ∥　　　　|";
-    let flag3 = "　　   ∥ 放不下 |";
+    let flag3 = "　　   ∥ 看上面 |";
     let flag4 = "　　   ∥￣￣￣￣";
     let cat1 = "　 ∧＿∧";
     let cat2 = "　(`･ω･∥";
